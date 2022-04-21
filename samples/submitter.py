c803382e79eac0ae8933686d8c40327d4238ad9a
sandbornm <michael.sandborn@vanderbilt.edu>
2022-04-20 19:46:55 -0500

    add config options in dispatcher

--- samples/submitter.py
+++ samples/submitter.py
@@ -9,11 +9,26 @@ l.setLevel(logging.INFO)
 
 task_id = []
 
+configs = ['win7x86_conf1',
+			'win7x86_conf2',
+			'win7x86_conf3',
+			'win7x86_conf4']
+			# 'winxpx64',
+			# 'winxpx86',
+			# 'win7x86',
+			# 'win7x64']
+
+config = os.environ['CONFIG']
+assert config in configs, f"{config} is invalid config"
+l.info(f"executing on config {config}")
+
 def submit():
+
+
     flist = glob.glob('./100_evasion/*/*.bin')
     for f in flist:
         subprocess.call(['cuckoo', 'submit', '--timeout', '240', '--machine',
-                         'win7x86_conf4', os.path.abspath(f)])
+				config, os.path.abspath(f)])
     #    task_id.append(db.add_path(file_path=os.path.abspath(f), timeout=60))
     #    r = requests.post()
     #return task_id
